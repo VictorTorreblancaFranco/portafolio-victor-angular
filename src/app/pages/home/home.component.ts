6bf771da7466b7b36f05e02830647cb34dbb0048
victor <victor.torreblanca.franco@vallegrande.edu.pe>
2025-05-09 15:17:28 -0500

    feat: update main

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
-// src/app/pages/home/home.component.ts
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';  // Importar MatIconModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [MatIconModule]  // Asegúrate de que MatIconModule esté en imports
 })
 export class HomeComponent {
   title = 'Bienvenido a mi portafolio';
