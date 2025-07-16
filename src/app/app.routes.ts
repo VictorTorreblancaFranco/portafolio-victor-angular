a370a76dee3c6ac171395dc1b6355e7390805eab
victor <victor.torreblanca.franco@vallegrande.edu.pe>
2025-07-16 00:22:35 -0500

    se separo el footer a un componente standalone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CertificationsComponent } from './pages/certifications/certifications.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { ProjectCarouselComponent } from './pages/project-carousel/project-carousel.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    // otras rutas
+    { path: 'certifications', component: CertificationsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'education', component: EducationComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'project-carousel', component: ProjectCarouselComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'testimonials', component: TestimonialsComponent },
+    // otras rutas si es necesario
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';  // Importar MatIconModule
+import { CommonModule } from '@angular/common'; // Importar CommonModule si es necesario
+import { FooterComponent } from '../../layout/footer/footer.component'; // Importar FooterComponent
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [MatIconModule]  // Asegúrate de que MatIconModule esté en imports
+  imports: [MatIconModule, FooterComponent, CommonModule]  // Asegúrate de incluir FooterComponent en imports
 })
 export class HomeComponent {
   title = 'Bienvenido a mi portafolio';
-    scrollToNext(): void {
+
+  scrollToNext(): void {
     window.scrollTo({
       top: window.innerHeight, // Desplaza hasta la parte inferior de la pantalla
       behavior: 'smooth' // Efecto de desplazamiento suave
